fcec53a6b25ddb1f82a19305d3a2b142c55acd54
akanksha-tech <akankshathalla21@gmail.com>
2022-08-08 22:35:02 +0530

    added a change

--- hungry.py
+++ hungry.py
@@ -1,6 +1,6 @@
 hungry = input("Are you hungry?")
 thirsty = input("Are you thirsy?")
-if hungry == "yes" and a="yo":
+if hungry == "yes" and thirsty=='no':
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
